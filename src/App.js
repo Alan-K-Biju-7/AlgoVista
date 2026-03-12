c9b7c24c06d178b44362578129a7df7561c564e0
Alan-K-Biju-7 <alan.k.biju25@gmail.com>
2026-03-12 21:25:02 +0530

    import the concepts page so it is ready to be wired into routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import MainLayout from './layout/MainLayout';
 import HomePage from './pages/HomePage';
+import ConceptsPage from './pages/ConceptsPage';
 import './styles/global.css';
 
 function App() {
   return (
     <MainLayout>
+      {/* swap between these manually for now while building */}
       <HomePage />
+      {/* <ConceptsPage /> */}
     </MainLayout>
   );
 }
