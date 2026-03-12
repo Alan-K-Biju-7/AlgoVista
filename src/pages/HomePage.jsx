879699da1935141adf0c93033f9767317fa0c6cf
Alan-K-Biju-7 <alan.k.biju25@gmail.com>
2026-03-12 21:19:15 +0530

    add a basic home page section for algovista

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+function HomePage() {
+  return (
+    <section>
+      <h1>AlgoVista</h1>
+      <p>
+        A visual lab for learning Data Structures and Algorithms step by step.
+      </p>
+    </section>
+  );
+}
+
+export default HomePage;
