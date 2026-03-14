b79a87e52ed5d532d7aeafd8b817a543505adced
Alan-K-Biju-7 <alan.k.biju25@gmail.com>
2026-03-14 08:56:15 +0530

    add sorting section with bubble and insertion visualizers

--- src/pages/SimulatorPage.jsx
+++ src/pages/SimulatorPage.jsx
@@ -4,8 +4,6 @@ import QueueVisualizer from '../modules/queue/QueueVisualizer';
 import BubbleSortVisualizer from '../modules/sorting/BubbleSortVisualizer';
 import InsertionSortVisualizer from '../modules/sorting/InsertionSortVisualizer';
 
-import InsertionSortVisualizer from '../modules/sorting/InsertionSortVisualizer';
-
 function SimulatorPage() {
   return (
     <div>
