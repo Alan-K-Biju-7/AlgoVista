8ecd2f2d9d49140f2a7a243dc227b9706410abc5
Alan-K-Biju-7 <alan.k.biju25@gmail.com>
2026-03-14 08:28:02 +0530

    set up basic routes for home concepts simulator and about pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './layout/MainLayout';
 import HomePage from './pages/HomePage';
 import ConceptsPage from './pages/ConceptsPage';
+import SimulatorPage from './pages/SimulatorPage';
+import AboutPage from './pages/AboutPage';
 import './styles/global.css';
 
 function App() {
   return (
-    <MainLayout>
-      {/* swap between these manually for now while building */}
-      <HomePage />
-      {/* <ConceptsPage /> */}
-    </MainLayout>
+    <Router>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/concepts" element={<ConceptsPage />} />
+          <Route path="/simulator" element={<SimulatorPage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </MainLayout>
+    </Router>
   );
 }
 
