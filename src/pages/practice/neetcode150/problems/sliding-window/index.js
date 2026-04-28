8860e8084d9a9eb62c97613dd176fe2f37ba1bbf
Alan-K-Biju-7 <alan.k.biju25@gmail.com>
2026-04-28 20:55:45 +0530

    Add Two Pointers and Sliding Window pattern indexes

---
+++ src/pages/practice/neetcode150/problems/sliding-window/index.js
@@ -0,0 +1,18 @@
+import bestTimeToBuyAndSellStock from './best-time-to-buy-and-sell-stock';
+import longestSubstringWithoutRepeatingCharacters from './longest-substring-without-repeating-characters';
+import longestRepeatingCharacterReplacement from './longest-repeating-character-replacement';
+import permutationInString from './permutation-in-string';
+import minimumWindowSubstring from './minimum-window-substring';
+import slidingWindowMaximum from './sliding-window-maximum';
+
+const SLIDING_WINDOW_PROBLEMS = [
+  bestTimeToBuyAndSellStock,
+  longestSubstringWithoutRepeatingCharacters,
+  longestRepeatingCharacterReplacement,
+  permutationInString,
+  minimumWindowSubstring,
+  slidingWindowMaximum,
+];
+
+export default SLIDING_WINDOW_PROBLEMS;
+export { SLIDING_WINDOW_PROBLEMS };
---
+++ src/pages/practice/neetcode150/problems/two-pointers/index.js
@@ -0,0 +1,16 @@
+import validPalindrome from './valid-palindrome';
+import twoSumII from './two-sum-ii-input-array-is-sorted';
+import threeSum from './3sum';
+import containerWithMostWater from './container-with-most-water';
+import trappingRainWater from './trapping-rain-water';
+
+const TWO_POINTERS_PROBLEMS = [
+  validPalindrome,
+  twoSumII,
+  threeSum,
+  containerWithMostWater,
+  trappingRainWater,
+];
+
+export default TWO_POINTERS_PROBLEMS;
+export { TWO_POINTERS_PROBLEMS };
