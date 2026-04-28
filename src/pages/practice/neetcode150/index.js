2f07a0ac14ab5408885039b6b9d58a649c72d788
Alan-K-Biju-7 <alan.k.biju25@gmail.com>
2026-04-28 20:56:27 +0530

    Register Two Pointers and Sliding Window in the NeetCode registry

--- src/pages/practice/neetcode150/index.js
+++ src/pages/practice/neetcode150/index.js
@@ -1,7 +1,11 @@
 import ARRAYS_HASHING_PROBLEMS from './problems/arrays-hashing';
+import TWO_POINTERS_PROBLEMS from './problems/two-pointers';
+import SLIDING_WINDOW_PROBLEMS from './problems/sliding-window';
 
 export const NEETCODE150 = [
   ...ARRAYS_HASHING_PROBLEMS,
+  ...TWO_POINTERS_PROBLEMS,
+  ...SLIDING_WINDOW_PROBLEMS,
 ];
 
 export function isNeetcode150(problemId) {
