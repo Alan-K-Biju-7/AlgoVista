3523289e75ada4a425deec65c763f01a7d9fbd17
Alan-K-Biju-7 <alan.k.biju25@gmail.com>
2026-04-30 20:04:16 +0530

    Add Binary Search problem index

---
+++ src/pages/practice/neetcode150/problems/binary-search/index.js
@@ -0,0 +1,20 @@
+import binarySearch from './binary-search';
+import searchA2DMatrix from './search-a-2d-matrix';
+import kokoEatingBananas from './koko-eating-bananas';
+import findMinimumInRotatedSortedArray from './find-minimum-in-rotated-sorted-array';
+import searchInRotatedSortedArray from './search-in-rotated-sorted-array';
+import timeBasedKeyValueStore from './time-based-key-value-store';
+import medianOfTwoSortedArrays from './median-of-two-sorted-arrays';
+
+const BINARY_SEARCH_PROBLEMS = [
+  binarySearch,
+  searchA2DMatrix,
+  kokoEatingBananas,
+  findMinimumInRotatedSortedArray,
+  searchInRotatedSortedArray,
+  timeBasedKeyValueStore,
+  medianOfTwoSortedArrays,
+];
+
+export default BINARY_SEARCH_PROBLEMS;
+export { BINARY_SEARCH_PROBLEMS };
