7b58616b6bd4453c969646a8f8883e8c0ad7efe7
Alan-K-Biju-7 <alan.k.biju25@gmail.com>
2026-04-30 19:52:27 +0530

    Register Stack problems in the NeetCode registry

--- src/pages/practice/neetcode150/index.js
+++ src/pages/practice/neetcode150/index.js
@@ -1,11 +1,13 @@
 import ARRAYS_HASHING_PROBLEMS from './problems/arrays-hashing';
 import TWO_POINTERS_PROBLEMS from './problems/two-pointers';
 import SLIDING_WINDOW_PROBLEMS from './problems/sliding-window';
+import STACK_PROBLEMS from './problems/stack';
 
 export const NEETCODE150 = [
   ...ARRAYS_HASHING_PROBLEMS,
   ...TWO_POINTERS_PROBLEMS,
   ...SLIDING_WINDOW_PROBLEMS,
+  ...STACK_PROBLEMS,
 ];
 
 export function isNeetcode150(problemId) {
