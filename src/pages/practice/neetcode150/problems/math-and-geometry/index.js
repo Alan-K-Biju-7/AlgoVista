04c2870e8c6a8018c370efad6c108a6c16730928
Alan K Biju <alan.k.biju25@gmail.com>
2026-05-25 15:24:00 +0530

    Add math and geometry problem exports

---
+++ src/pages/practice/neetcode150/problems/math-and-geometry/index.js
@@ -0,0 +1,24 @@
+import rotateImage from './rotate-image';
+import spiralMatrix from './spiral-matrix';
+import setMatrixZeroes from './set-matrix-zeroes';
+import happyNumber from './happy-number';
+import plusOne from './plus-one';
+import powxN from './powx-n';
+import multiplyStrings from './multiply-strings';
+import detectSquares from './detect-squares';
+import reverseInteger from './reverse-integer';
+
+const MATH_AND_GEOMETRY_PROBLEMS = [
+  rotateImage,
+  spiralMatrix,
+  setMatrixZeroes,
+  happyNumber,
+  plusOne,
+  powxN,
+  multiplyStrings,
+  detectSquares,
+  reverseInteger,
+];
+
+export default MATH_AND_GEOMETRY_PROBLEMS;
+export { MATH_AND_GEOMETRY_PROBLEMS };
