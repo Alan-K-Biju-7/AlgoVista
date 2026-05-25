7fbd1d25bc3cf8d328c9bf3baaa9483d4980524b
Alan K Biju <alan.k.biju25@gmail.com>
2026-05-25 15:24:00 +0530

    Add dynamic programming problem exports

---
+++ src/pages/practice/neetcode150/problems/dynamic-programming/index.js
@@ -0,0 +1,30 @@
+import climbingStairs from './climbing-stairs';
+import minCostClimbingStairs from './min-cost-climbing-stairs';
+import houseRobber from './house-robber';
+import houseRobberII from './house-robber-ii';
+import longestPalindromicSubstring from './longest-palindromic-substring';
+import palindromicSubstrings from './palindromic-substrings';
+import decodeWays from './decode-ways';
+import coinChange from './coin-change';
+import maximumProductSubarray from './maximum-product-subarray';
+import wordBreak from './word-break';
+import longestIncreasingSubsequence from './longest-increasing-subsequence';
+import partitionEqualSubsetSum from './partition-equal-subset-sum';
+
+const DYNAMIC_PROGRAMMING_PROBLEMS = [
+  climbingStairs,
+  minCostClimbingStairs,
+  houseRobber,
+  houseRobberII,
+  longestPalindromicSubstring,
+  palindromicSubstrings,
+  decodeWays,
+  coinChange,
+  maximumProductSubarray,
+  wordBreak,
+  longestIncreasingSubsequence,
+  partitionEqualSubsetSum,
+];
+
+export default DYNAMIC_PROGRAMMING_PROBLEMS;
+export { DYNAMIC_PROGRAMMING_PROBLEMS };
