768797c7c8558ef959e7829ab9eb0df6300c6f34
Alan K Biju <alan.k.biju25@gmail.com>
2026-05-25 15:24:00 +0530

    Add heap priority queue problem exports

---
+++ src/pages/practice/neetcode150/problems/heap-priority-queue/index.js
@@ -0,0 +1,20 @@
+import kthLargestElementInAStream from './kth-largest-element-in-a-stream';
+import lastStoneWeight from './last-stone-weight';
+import kClosestPointsToOrigin from './k-closest-points-to-origin';
+import kthLargestElementInAnArray from './kth-largest-element-in-an-array';
+import taskScheduler from './task-scheduler';
+import designTwitter from './design-twitter';
+import findMedianFromDataStream from './find-median-from-data-stream';
+
+const HEAP_PRIORITY_QUEUE_PROBLEMS = [
+  kthLargestElementInAStream,
+  lastStoneWeight,
+  kClosestPointsToOrigin,
+  kthLargestElementInAnArray,
+  taskScheduler,
+  designTwitter,
+  findMedianFromDataStream,
+];
+
+export default HEAP_PRIORITY_QUEUE_PROBLEMS;
+export { HEAP_PRIORITY_QUEUE_PROBLEMS };
