ecee9cb281f5e169daa969ee62b544b6af638d6b
Alan K Biju <alan.k.biju25@gmail.com>
2026-05-25 15:24:00 +0530

    Add greedy problem exports

---
+++ src/pages/practice/neetcode150/problems/greedy/index.js
@@ -0,0 +1,22 @@
+import maximumSubarray from './maximum-subarray';
+import jumpGame from './jump-game';
+import jumpGameII from './jump-game-ii';
+import gasStation from './gas-station';
+import handOfStraights from './hand-of-straights';
+import mergeTripletsToFormTargetTriplet from './merge-triplets-to-form-target-triplet';
+import partitionLabels from './partition-labels';
+import validParenthesisString from './valid-parenthesis-string';
+
+const GREEDY_PROBLEMS = [
+  maximumSubarray,
+  jumpGame,
+  jumpGameII,
+  gasStation,
+  handOfStraights,
+  mergeTripletsToFormTargetTriplet,
+  partitionLabels,
+  validParenthesisString,
+];
+
+export default GREEDY_PROBLEMS;
+export { GREEDY_PROBLEMS };
