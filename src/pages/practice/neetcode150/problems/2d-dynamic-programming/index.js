a9a853249efb448c1f6ea4bccbe644880c012fea
Alan K Biju <alan.k.biju25@gmail.com>
2026-05-25 15:23:59 +0530

    Add two-dimensional DP problem exports

---
+++ src/pages/practice/neetcode150/problems/2d-dynamic-programming/index.js
@@ -0,0 +1,28 @@
+import uniquePaths from './unique-paths';
+import longestCommonSubsequence from './longest-common-subsequence';
+import bestTimeToBuyAndSellStockWithCooldown from './best-time-to-buy-and-sell-stock-with-cooldown';
+import coinChangeII from './coin-change-ii';
+import targetSum from './target-sum';
+import interleavingString from './interleaving-string';
+import longestIncreasingPathInAMatrix from './longest-increasing-path-in-a-matrix';
+import distinctSubsequences from './distinct-subsequences';
+import editDistance from './edit-distance';
+import burstBalloons from './burst-balloons';
+import regularExpressionMatching from './regular-expression-matching';
+
+const TWO_D_DYNAMIC_PROGRAMMING_PROBLEMS = [
+  uniquePaths,
+  longestCommonSubsequence,
+  bestTimeToBuyAndSellStockWithCooldown,
+  coinChangeII,
+  targetSum,
+  interleavingString,
+  longestIncreasingPathInAMatrix,
+  distinctSubsequences,
+  editDistance,
+  burstBalloons,
+  regularExpressionMatching,
+];
+
+export default TWO_D_DYNAMIC_PROGRAMMING_PROBLEMS;
+export { TWO_D_DYNAMIC_PROGRAMMING_PROBLEMS };
